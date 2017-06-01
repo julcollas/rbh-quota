ec731d59c551533c65177bdd529b68f7f54a4e7a
root <root@dhcpvm4-54.install.fr.cfm.fr>
2017-06-01 11:02:10 +0200

    Removing print

--- rbh_quota/rbhQuota.py
+++ rbh_quota/rbhQuota.py
@@ -105,12 +105,12 @@ def insert():
         user = db.fetchall()
 	i = 0
         while (i < len(user)):
-            p = subprocess.Popen(["/usr/bin/lfs", "quota", "-u", user[i][0], fs_path], stdout=subprocess.PIPE)
+            p = subprocess.Popen(["lfs", "quota", "-u", user[i][0], fs_path], stdout=subprocess.PIPE)
 	    out = p.communicate()[0].replace('\n', ' ')
-	    print(out)
 	    values = re.findall('([\d]+|\-)\s(?![(]uid)', out)
-	    print(values)
-            db.execute("INSERT INTO QUOTA VALUES('" + user[i][0] + "', " + values[1] + ", " + values[2] + ", " + values[5] + ", " + values[6] + ")")
+            db.execute("INSERT INTO QUOTA VALUES('" + user[i][0] + 
+				"', " + values[1] + ", " + values[2] + 	
+				", " + values[5] + ", " + values[6] + ")")
 	    i += 1
 
     try:
